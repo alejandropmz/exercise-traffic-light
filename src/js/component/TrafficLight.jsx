695134c2de4b7bf2375b013e81d331053587fd9f
alejandropmz <alejandropacheco2497@gmail.com>
2022-11-21 03:41:43 +0000

    sunday practice

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -1,7 +1,8 @@
 import React, {useState} from "react";
 
-const Trafficlight = (props) => {
+const Trafficlight = () => {
 
+  const colors = ["red", "yellow", "green"];
   const [color, setColor] = useState("");
   
   return (
@@ -9,13 +10,21 @@ const Trafficlight = (props) => {
       <div className="semaphore">
         <div className="tube"></div>
         <div className="traffic-light">
-          <div className="light red"></div>
-          <div className="light yellow"></div>
-          <div className="light green"></div>
+          <div onClick={()=> setColor("red")} className={color == "red" ? "light red":"off light red"}></div>
+          <div onClick={()=> setColor("yellow")} className={color == "yellow" ? "light yellow":"off light yellow"}></div>
+          <div onClick={()=> setColor("green")} className={color == "green" ? "light green":"off light green"}></div>
+          <button onClick={()=>{
+            let number = Math.floor(Math.random()*3)
+            setColor(colors[number])
+          }} className="btn btn-primary"></button>
         </div>
+        <div className="tube"></div>
+        <button className="button btn btn-success"></button>
       </div>
     </div>
   );
 };
+
+
 
 export default Trafficlight;
